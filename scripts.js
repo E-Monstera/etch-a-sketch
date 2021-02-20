b895b37690ed05f4c1fb7d18e7e453c82d25bd69
Elisabeth Kittle <elisabethkittle@gmail.com>
2021-02-20 17:03:22 -0500

    Color Changes Added

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
 let size = 16;
-const sketchPad = document.querySelector('.sketchy');   //used to set the dimensions of the sketchpad
+let newColor = 'blue';
+const sketchPad = document.querySelector('.sketchy');       //used to set the dimensions of the sketchpad
+const colorPicker = document.querySelector('#pixelColor');  //Constant for the color picker
+const blackButton = document.querySelector('#blackButton'); //Constant for button that changes the pixels black
+const clearButton = document.querySelector('#clearButton'); //Constant for the clear button
 
 //Set the grid to 16x16 by default
 function populateGridDefault(){
@@ -15,11 +19,27 @@ function populateGridDefault(){
 }
 
 
+//Triggered by the event listener to change the color of the pixels
+function colorChange(e){
+    newColor = this.value;
+}
+
+function changeToBlack(){
+    newColor = '#000000';
+}
+
+function clearGrid(){ 
+    let clear = 'white';
+    divs.forEach((div) => {
+        div.style.backgroundColor = clear;
+        console.log("nope");
+    });
+    // divs.forEach(div => div.backgroundColor = clear);
+}
 
 //Adds the new class/color to the div
 function updateDIV(e){
-    console.log(e);
-    let newColor = 'blue';
+    // console.log(e);
     this.style.backgroundColor = newColor;
 }
 
@@ -33,10 +53,18 @@ function populateGrid(newSize){
 
 
 
-
+//Populate the grid with a 16x16 grid
 populateGridDefault();
+
+//Set the color of the pixels
+colorPicker.addEventListener('change', colorChange);
+blackButton.addEventListener('click', changeToBlack);
+clearButton.addEventListener('click', clearGrid);
+
+
 const divs = document.querySelectorAll('.box');
 divs.forEach(box => box.addEventListener('mouseover', updateDIV));
+
 
 
 //Add a class to the current div that sets it up as a grid w/ 16rows/columns
