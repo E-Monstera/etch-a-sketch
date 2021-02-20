46adfafe6d47f08630c0d403887e23baff7e1eee
Elisabeth Kittle <elisabethkittle@gmail.com>
2021-02-20 17:50:26 -0500

    Added pixel ranges

--- scripts.js
+++ scripts.js
@@ -4,18 +4,22 @@ const sketchPad = document.querySelector('.sketchy');       //used to set the di
 const colorPicker = document.querySelector('#pixelColor');  //Constant for the color picker
 const blackButton = document.querySelector('#blackButton'); //Constant for button that changes the pixels black
 const clearButton = document.querySelector('#clearButton'); //Constant for the clear button
+const rangeSlider = document.querySelector('#pixelRange');        //Constant for the slider
+
+
 
 //Set the grid to 16x16 by default
-function populateGridDefault(){
-    sketchPad.setAttribute('style', 'display: grid; grid-template-columns: repeat(16, 1fr); grid-template-rows: repeat(16, 1fr); border-style: solid;');
-    let divs = 16 * 16;
+function populateGrid(pixels){
+        sketchPad.setAttribute('style', 'display: grid; grid-template-columns: repeat('+pixels+', 1fr); grid-template-rows: repeat('+pixels+', 1fr); border-style: solid;');
+    let divs = pixels * pixels;
     while (divs > 0){
         let content = document.createElement('div');
         content.classList.add('box');
         sketchPad.appendChild(content);
-        console.log('this worked');
         divs--;
     }
+    const newDivs = document.querySelectorAll('.box');
+    newDivs.forEach(box => box.addEventListener('mouseover', updateDIV));
 }
 
 
@@ -24,6 +28,8 @@ function colorChange(e){
     newColor = this.value;
 }
 
+
+//
 function changeToBlack(){
     newColor = '#000000';
 }
@@ -45,16 +51,19 @@ function updateDIV(e){
 
 
 //Update when adding sizes to the grid
-function populateGrid(newSize){
-    let newGrid = document.createElement('div');
-    newGrid.style.cssText = 'display: grid; grid-template-columns: repeat(newSize, 1fr); grid-template-rows: repeat(newSize, 1fr); border-style: solid;';
-    sketchPad.appendChild(newGrid);
+function updateTable(e){
+    console.log(this.value);
+    sketchPad.innerHTML = "";
+    populateGrid(this.value);
+    // let newGrid = document.createElement('div');
+    // newGrid.style.cssText = 'display: grid; grid-template-columns: repeat(newSize, 1fr); grid-template-rows: repeat(newSize, 1fr); border-style: solid;';
+    // sketchPad.appendChild(newGrid);
 }
 
 
 
 //Populate the grid with a 16x16 grid
-populateGridDefault();
+populateGrid(16);
 
 //Set the color of the pixels
 colorPicker.addEventListener('change', colorChange);
@@ -62,10 +71,8 @@ blackButton.addEventListener('click', changeToBlack);
 clearButton.addEventListener('click', clearGrid);
 
 
-const divs = document.querySelectorAll('.box');
-divs.forEach(box => box.addEventListener('mouseover', updateDIV));
-
-
+//Slider
+rangeSlider.addEventListener('change', updateTable);
 
 //Add a class to the current div that sets it up as a grid w/ 16rows/columns
 //Then create 16*16 divs to fill that grid - Add a class to these grids
