cacbf743d7cd1c8357517617bc8e41796ffb1e12
Elisabeth Kittle <elisabethkittle@gmail.com>
2021-02-22 14:28:29 -0500

    Added slider and button functionality

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 let size = 16;
 let newColor = '#F9BF7C';
+let rainbow = false;
 const sketchPad = document.querySelector('.sketchy');       //used to set the dimensions of the sketchpad
 const colorPicker = document.querySelector('#pixelColor');  //Constant for the color picker
 const blackButton = document.querySelector('#blackButton'); //Constant for button that changes the pixels black
 const clearButton = document.querySelector('#clearButton'); //Constant for the clear button
 const rangeSlider = document.querySelector('#pixelRange');        //Constant for the slider
+const rainbowButton = document.querySelector('#rainbowButton'); //Constant for the rainbow button
 
 
 
@@ -26,12 +28,29 @@ function populateGrid(pixels){
 //Triggered by the event listener to change the color of the pixels
 function colorChange(e){
     newColor = this.value;
+    rainbow = false;
 }
 
 
 //
 function changeToBlack(){
     newColor = '#000000';
+    rainbow = false;
+}
+
+
+//Use a random number generator to choose a random color for the pixel
+function rainbowChange(){
+    let colorNum1 = getRandomInt(255);
+    let colorNum2 = getRandomInt(255);
+    let colorNum3 = getRandomInt(255);
+    newColor = 'rgb('+colorNum1+', '+colorNum2+', '+colorNum3+')';
+    rainbow = true;
+}
+
+//Random int function for the rainbow pixels
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
 }
 
 function clearGrid(){ 
@@ -39,14 +58,16 @@ function clearGrid(){
     const newDivs = document.querySelectorAll('.box');
     newDivs.forEach((div) => {
         div.style.backgroundColor = clear;
-        console.log("nope");
     });
     // divs.forEach(div => div.backgroundColor = clear);
 }
 
 //Adds the new class/color to the div
 function updateDIV(e){
-    // console.log(e);
+    console.log(e);
+    if (rainbow){
+        rainbowChange();
+    }
     this.style.backgroundColor = newColor;
 }
 
@@ -56,9 +77,6 @@ function updateTable(e){
     console.log(this.value);
     sketchPad.innerHTML = "";
     populateGrid(this.value);
-    // let newGrid = document.createElement('div');
-    // newGrid.style.cssText = 'display: grid; grid-template-columns: repeat(newSize, 1fr); grid-template-rows: repeat(newSize, 1fr); border-style: solid;';
-    // sketchPad.appendChild(newGrid);
 }
 
 
@@ -70,11 +88,7 @@ populateGrid(16);
 colorPicker.addEventListener('change', colorChange);
 blackButton.addEventListener('click', changeToBlack);
 clearButton.addEventListener('click', clearGrid);
-
+rainbowButton.addEventListener('click', rainbowChange);
 
 //Slider
 rangeSlider.addEventListener('change', updateTable);
-
-//Add a class to the current div that sets it up as a grid w/ 16rows/columns
-//Then create 16*16 divs to fill that grid - Add a class to these grids
-//Then, create a CSS attribute using divClass:hover to add the effects
